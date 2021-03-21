3dc1b80c1edab84eb24bc3f1530fa1495381b1ab
ta264 <ta264@users.noreply.github.com>
2021-03-21 21:29:12 +0000

    Housekeeper to migrate deemix branch

---
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/FixDeemixDelayProfile.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using NzbDrone.Core.Datastore;
+
+namespace NzbDrone.Core.Housekeeping.Housekeepers
+{
+    public class FixDeemixDelayProfile : IHousekeepingTask
+    {
+        private readonly IMainDatabase _database;
+
+        public FixDeemixDelayProfile(IMainDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Clean()
+        {
+            using (var mapper = _database.OpenConnection())
+            {
+                mapper.Execute(@"UPDATE DelayProfiles
+                                 SET Items = REPLACE(Items, 'usenet', 'UsenetDownloadProtocol')");
+                mapper.Execute(@"UPDATE DelayProfiles
+                                 SET Items = REPLACE(Items, 'torrent', 'TorrentDownloadProtocol')");
+                mapper.Execute(@"UPDATE DelayProfiles
+                                 SET Items = REPLACE(Items, 'deemix', 'DeemixDownloadProtocol')");
+            }
+        }
+    }
+}

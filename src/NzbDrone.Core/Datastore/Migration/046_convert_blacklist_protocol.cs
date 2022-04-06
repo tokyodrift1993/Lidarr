fd38ae720f1f028aa0fbd3a053116b4e39149505
ta264 <ta264@users.noreply.github.com>
2022-04-06 19:38:22 +0100

    fixup! New: Plugin support

--- src/NzbDrone.Core/Datastore/Migration/046_convert_blacklist_protocol.cs
+++ src/NzbDrone.Core/Datastore/Migration/046_convert_blacklist_protocol.cs
@@ -8,11 +8,14 @@ namespace NzbDrone.Core.Datastore.Migration
     {
         protected override void MainDbUpgrade()
         {
-            Alter.Table("Blacklist").AlterColumn("Protocol").AsString().Nullable();
+            // This may get run after the blacklist -> blocklist migration
+            var table = Schema.Table("Blacklist").Exists() ? "Blacklist" : "Blocklist";
 
-            Execute.Sql(@"DELETE FROM Blacklist WHERE Protocol=0");
-            Execute.Sql(@"UPDATE Blacklist SET Protocol='UsenetDownloadProtocol' WHERE Protocol=1");
-            Execute.Sql(@"UPDATE Blacklist SET Protocol='TorrentDownloadProtocol' WHERE Protocol=2");
+            Alter.Table(table).AlterColumn("Protocol").AsString().Nullable();
+
+            Execute.Sql($"DELETE FROM {table} WHERE Protocol=0");
+            Execute.Sql($"UPDATE {table} SET Protocol='UsenetDownloadProtocol' WHERE Protocol=1");
+            Execute.Sql($"UPDATE {table} SET Protocol='TorrentDownloadProtocol' WHERE Protocol=2");
         }
     }
 }
